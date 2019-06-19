bb52ba37b2b29dee07e1f23d2f717ba71e8c357f
mchugh16 <mira.chugh@checkr.com>
2019-06-18 17:28:16 -0700

    Part 1 work through flask blueprints

---
+++ services/users/project/tests/test_users.py
@@ -0,0 +1,44 @@
+import json
+import unittest
+
+from project.tests.base import BaseTestCase
+
+
+class TestUserService(BaseTestCase):
+    """Tests for the Users Service."""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly."""
+        response = self.client.get('/users/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        """Ensure a new user can be added to the db"""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'michael',
+                    'email': 'michael@mherman.org'
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('michael@mherman.org was added!', data['message'])
+            self.assertIn('success', data['status'])
+    
+    # def test_add_user_invalid_json (self):
+    #     """Ensure error is thrown if the JSON object is empty
+    #         i.e. the payload is not sent.
+    #     """
+        
+    
+
+
+
+if __name__ == '__main__':
+    unittest.main()
